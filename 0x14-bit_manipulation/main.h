5b157aa832d154f57da8b001ba81afefc2465b0b
vikkirkobane <victorchogo37@gmail.com>
2022-11-01 06:38:02 -0700

    get endianness

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,6 @@
 #ifndef _BIT_MANIP_
 #define _BIT_MANIP_
+#include <stddef.h>
 
 int _strlen(const char *s);
 unsigned int binary_to_uint(const char *b);
@@ -10,5 +11,6 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
 #endif
